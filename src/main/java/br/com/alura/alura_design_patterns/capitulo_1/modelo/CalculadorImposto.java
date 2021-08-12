cab4287767cb7d199f36d8fabece8bf80121537d
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-08-12 18:14:02 -0300

    com essa nova implementação para cada tipo de imposto um novo método deveria ser criado; ainda não é a melhor estratégia

--- src/main/java/br/com/alura/alura_design_patterns/capitulo_1/modelo/CalculadorImposto.java
+++ src/main/java/br/com/alura/alura_design_patterns/capitulo_1/modelo/CalculadorImposto.java
@@ -1,35 +1,32 @@
 package br.com.alura.alura_design_patterns.capitulo_1.modelo;
 
 /**
- * #Contexto:
- * É necessário calcular o valor do imposto de acordo com o
- * imposto recebido no método realizarCalculo, que recebe o orçamento
- * e o tipo de imposto a ser calculado sobre o parâmetro orçamento.
+ * #Contexto: É necessário calcular o valor do imposto de acordo com o imposto
+ * recebido no método realizarCalculo, que recebe o orçamento e o tipo de
+ * imposto a ser calculado sobre o parâmetro orçamento.
  *
- * Dessa forma o método teria que ser alterado a cado novo imposto
- * que fosse adicionado ao sistema. Sendo necessário fazer diversos if's
- * para cada um dos tipos de imposto.
+ * Dessa forma o método teria que ser alterado a cado novo imposto que fosse
+ * adicionado ao sistema. Sendo necessário fazer diversos if's para cada um dos
+ * tipos de imposto.
  * 
- * Pensando nos princípios S.O.L.I.D, está classe não estaria respeitando
- * o princípio de Single Responsability (<strong>S</strong>),
- * e o princípio Open-Closed (<strong>O</strong>).
+ * Pensando nos princípios S.O.L.I.D, está classe não estaria respeitando o
+ * princípio de Single Responsability (<strong>S</strong>), e o princípio
+ * Open-Closed (<strong>O</strong>).
  * 
- * #Solução:
- * Separar as responsabilidades em classes, para que cada uma delas fique
- * mais coesa, desempenhando somente uma tarefa.
+ * #Solução: Separar as responsabilidades em classes, para que cada uma delas
+ * fique mais coesa, desempenhando somente uma tarefa.
  * 
  * @author Pedro Junior
  *
  */
 public class CalculadorImposto {
-	public void realizarCalculo(Orcamento orcamento, String imposto) {
-		if ("ICMS".equals(imposto)) {
-			double icms = new ICMS().calcularICMS(orcamento);
-			System.out.println(icms);			
-		}
-		else if ("ISS".equals(imposto)) {
-			double iss = new ISS().calcularISS(orcamento);
-			System.out.println(iss);			
-		}
+	public void realizarCalculoICMS(Orcamento orcamento) {
+		double icms = new ICMS().calcularICMS(orcamento);
+		System.out.println(icms);
+	}
+
+	public void realizarCalculoISS(Orcamento orcamento) {
+		double iss = new ISS().calcularISS(orcamento);
+		System.out.println(iss);
 	}
 }
