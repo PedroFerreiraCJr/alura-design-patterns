c319c5eb39c7da448d9eb965ed16672a483747c8
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-08-12 22:37:34 -0300

    adição de novas implementações de imposto

--- src/main/java/br/com/alura/alura_design_patterns/capitulo_1/Main.java
+++ src/main/java/br/com/alura/alura_design_patterns/capitulo_1/Main.java
@@ -1,14 +1,20 @@
 package br.com.alura.alura_design_patterns.capitulo_1;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import br.com.alura.alura_design_patterns.capitulo_1.modelo.ICMS;
+import br.com.alura.alura_design_patterns.capitulo_1.modelo.ICPP;
+import br.com.alura.alura_design_patterns.capitulo_1.modelo.IKCV;
 import br.com.alura.alura_design_patterns.capitulo_1.modelo.ISS;
 import br.com.alura.alura_design_patterns.capitulo_1.modelo.Imposto;
 import br.com.alura.alura_design_patterns.capitulo_1.modelo.Orcamento;
 import br.com.alura.alura_design_patterns.capitulo_1.service.CalculadorImposto;
 
 public class Main {
+	private static final List<String> CODIGO_DO_IMPOSTO = Arrays.asList("1", "2", "3", "4");
+
 	public static void main(String[] args) {
 		final CalculadorImposto calculador = new CalculadorImposto();
 
@@ -21,6 +27,8 @@ public class Main {
 				System.out.println("Escolha o tipo de imposto:");
 				System.out.println("1. :: ISS");
 				System.out.println("2. :: ICMS");
+				System.out.println("3. :: ICPP");
+				System.out.println("4. :: IKCV");
 				System.out.println();
 				System.out.println();
 				System.out.println("Digite 'exit' para sair.");
@@ -31,9 +39,9 @@ public class Main {
 					break outer;
 				}
 
-				if (!"1".equals(option.trim()) && !"2".equals(option.trim())) {
+				if (!CODIGO_DO_IMPOSTO.contains(option.trim())) {
 					System.out.println("Imposto inválido.");
-					System.out.println("Selecione uma das opções: 1 ou 2.");
+					System.out.println("Selecione uma das opções: 1, 2, 3, 4.");
 					continue;
 				}
 
@@ -60,6 +68,10 @@ public class Main {
 		Imposto imposto = new ISS();
 		if ("2".equals(option)) {
 			imposto = new ICMS();
+		} else if ("3".equals(option)) {
+			imposto = new ICPP();
+		} else if ("4".equals(option)) {
+			imposto = new IKCV();
 		}
 		return new OrcamentoEImposto(orcamento, imposto);
 	}
